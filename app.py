98ce6f41df6a7a977ae02292e07f012dadf65f03
Katherine <53545200+KatRootes@users.noreply.github.com>
2019-11-10 19:58:00 -0600

    Minor changes.  Still trying to get Data page correct.

--- app.py
+++ app.py
@@ -7,5 +7,8 @@ app = Flask(__name__)
 def data():
     path = "Resources/cities.csv"
     df = pd.read_csv(path)
-    html = pd.DataFrame.to_html(df)
+    html = df.to_html().replace('<table border="1" class="dataframe">','<table class="table table-striped">')
+    f = open('Resources/cities.html','w')
+    f.write(html)
+    f.close()
     return html
