87c9d7e42cae8ca1dcaa5206fc625057f2e395c1
Katherine <53545200+KatRootes@users.noreply.github.com>
2019-11-10 18:31:15 -0600

    Got Comparison working.  Got html working on its own with app.py.  Can't connect data.html and app.py

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route("data")
+@app.route("/data")
 def data():
-    path = "/Resources/cities.csv"
+    path = "Resources/cities.csv"
     df = pd.read_csv(path)
     html = pd.DataFrame.to_html(df)
     return html
